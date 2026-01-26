3881e346b91acc812cd3bf7ab95560ddd26d4b8e
Erik Berlin <sferik@gmail.com>
2026-01-26 15:04:57 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gemspec
+
+group :development, :test do
+  gem "minitest", ">= 6"
+  gem "rake", ">= 13.3"
+  gem "simplecov", ">= 0.22", require: false
+
+  # Linting
+  gem "rubocop", ">= 1.82"
+  gem "rubocop-minitest", ">= 0.38"
+  gem "rubocop-performance", ">= 1.26"
+  gem "rubocop-rake", ">= 0.7"
+  gem "standard", ">= 1.53"
+  gem "standard-performance", ">= 1.9"
+
+  # Type checking
+  gem "steep", ">= 1.10"
+
+  # Documentation
+  gem "yard", ">= 0.9"
+  gem "yardstick", ">= 0.9"
+
+  # Mutation testing
+  gem "mutant-minitest", ">= 0.14"
+end
---
+++ test/deconstruct_test.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class DeconstructTest < EquitableTestCase
+  def test_deconstruct
+    assert_equal [1, 2], Point.new(1, 2).deconstruct
+
+    case Point.new(3, 4)
+    in [x, y] then assert_equal 7, x + y
+    end
+  end
+
+  def test_deconstruct_keys
+    point = Point.new(1, 2, "label")
+
+    assert_equal({x: 1, y: 2}, point.deconstruct_keys(nil))
+    assert_equal({x: 1}, point.deconstruct_keys([:x]))
+    assert_equal({x: 1}, point.deconstruct_keys(%i[x unknown]))
+    assert_empty point.deconstruct_keys([:unknown])
+  end
+
+  def test_deconstruct_keys_pattern_matching
+    case Point.new(3, 4)
+    in {x:, y:} then assert_equal 12, x * y
+    end
+  end
+
+  def test_deconstruct_returns_values_in_order
+    klass = Class.new do
+      include Equitable.new(:a, :b, :c)
+
+      attr_reader :a, :b, :c
+      def initialize(a, b, c) = (@a, @b, @c = a, b, c)
+    end
+
+    assert_equal [1, 2, 3], klass.new(1, 2, 3).deconstruct
+  end
+
+  def test_deconstruct_keys_returns_requested_keys
+    klass = Class.new do
+      include Equitable.new(:a, :b)
+
+      attr_reader :a, :b
+      def initialize(a, b) = (@a, @b = a, b)
+    end
+
+    obj = klass.new(1, 2)
+
+    assert_equal({a: 1, b: 2}, obj.deconstruct_keys(nil))
+    assert_equal({a: 1}, obj.deconstruct_keys([:a]))
+    assert_equal({b: 2}, obj.deconstruct_keys([:b]))
+    assert_empty obj.deconstruct_keys([:unknown])
+  end
+end
